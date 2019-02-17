f223b26b11c7820ff30529a09f771ddf865bc88b
Braden Walker <bradenawalker@gmail.com>
2019-02-16 21:33:13 -0800

    OrdVarint64

--- encode.go
+++ encode.go
@@ -35,6 +35,7 @@ import (
 
 var ErrOverflowVarint = errors.New("encode: overflowed varint")
 var ErrInvalidBool = errors.New("encode: invalid bool, encoded value not 0 or 1")
+var ErrInvalidVarint = errors.New("encode: invalid varint")
 
 type Item interface {
 	// Encode this item into buf. buf will be at least Size() bytes.
@@ -369,6 +370,374 @@ func (e ordUvarint64) Decode(buf []byte) error {
 	}
 	mask := (uint64(1) << uint(rBits)) - 1
 	*e.v = result & mask
+	return nil
+}
+
+// Similar to Varint64, produces a variable-length encoding for v. However, OrdVarint64's encoding
+// lexicographically orders in the same order as the input.
+//
+// The encoding places the sign in the higest-order bit, with 0 meaning negative, so that negative
+// numbers order before all positive numbers. Leading zeroes for positive numbers and leading ones
+// for negative numbers are left off, encoding only the k lowest-order bits according to the
+// following scheme:
+//
+//   min     max          encoded size     encoding, where x is an input bit
+//   -2^63   -2^48 + 1    9                00000000 0xxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx
+//   -2^55   -2^48 + 1    8                00000000 1xxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx
+//   -2^48   -2^41 + 1    7                00000001 xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx
+//   -2^41   -2^34 + 1    6                0000001x xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx
+//   -2^34   -2^27 + 1    5                000001xx xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx
+//   -2^27   -2^20 + 1    4                00001xxx xxxxxxxx xxxxxxxx xxxxxxxx
+//   -2^20   -2^13 + 1    3                0001xxxx xxxxxxxx xxxxxxxx
+//   -2^13   -2^6 + 1     2                001xxxxx xxxxxxxx
+//   -2^6    -1           1                01xxxxxx
+//   0       2^6 - 1      1                10xxxxxx
+//   2^6     2^13 - 1     2                110xxxxx xxxxxxxx
+//   2^13    2^20 - 1     3                1110xxxx xxxxxxxx xxxxxxxx
+//   2^20    2^27 - 1     4                11110xxx xxxxxxxx xxxxxxxx xxxxxxxx
+//   2^27    2^34 - 1     5                111110xx xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx
+//   2^34    2^41 - 1     6                1111110x xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx
+//   2^41    2^48 - 1     7                11111110 xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx
+//   2^48    2^55 - 1     8                11111111 0xxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx
+//   2^48    2^63 - 1     9                11111111 1xxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx
+func OrdVarint64(v *int64) Item {
+	return ordVarint64{v}
+}
+
+type ordVarint64 struct{ v *int64 }
+
+func (e ordVarint64) Encode(buf []byte) {
+	switch {
+	case *e.v <= -(1<<55)+1:
+		buf[0] = 0x00
+		buf[1] = byte(*e.v>>56) & 0x7F
+		buf[2] = byte(*e.v >> 48)
+		buf[3] = byte(*e.v >> 40)
+		buf[4] = byte(*e.v >> 32)
+		buf[5] = byte(*e.v >> 24)
+		buf[6] = byte(*e.v >> 16)
+		buf[7] = byte(*e.v >> 8)
+		buf[8] = byte(*e.v)
+	case -(1<<55) <= *e.v && *e.v <= -(1<<48)+1:
+		buf[0] = 0x00
+		buf[1] = 0x80 | byte(*e.v>>48)
+		buf[2] = byte(*e.v >> 40)
+		buf[3] = byte(*e.v >> 32)
+		buf[4] = byte(*e.v >> 24)
+		buf[5] = byte(*e.v >> 16)
+		buf[6] = byte(*e.v >> 8)
+		buf[7] = byte(*e.v)
+	case -(1<<48) <= *e.v && *e.v <= -(1<<41)+1:
+		buf[0] = 0x01
+		buf[1] = byte(*e.v >> 40)
+		buf[2] = byte(*e.v >> 32)
+		buf[3] = byte(*e.v >> 24)
+		buf[4] = byte(*e.v >> 16)
+		buf[5] = byte(*e.v >> 8)
+		buf[6] = byte(*e.v)
+	case -(1<<41) <= *e.v && *e.v <= -(1<<34)+1:
+		buf[0] = 0x02 | (byte(*e.v>>40) & 0x01)
+		buf[1] = byte(*e.v >> 32)
+		buf[2] = byte(*e.v >> 24)
+		buf[3] = byte(*e.v >> 16)
+		buf[4] = byte(*e.v >> 8)
+		buf[5] = byte(*e.v)
+	case -(1<<34) <= *e.v && *e.v <= -(1<<27)+1:
+		buf[0] = 0x04 | (byte(*e.v>>32) & 0x03)
+		buf[1] = byte(*e.v >> 24)
+		buf[2] = byte(*e.v >> 16)
+		buf[3] = byte(*e.v >> 8)
+		buf[4] = byte(*e.v)
+	case -(1<<27) <= *e.v && *e.v <= -(1<<20)+1:
+		buf[0] = 0x08 | (byte(*e.v>>24) & 0x07)
+		buf[1] = byte(*e.v >> 16)
+		buf[2] = byte(*e.v >> 8)
+		buf[3] = byte(*e.v)
+	case -(1<<20) <= *e.v && *e.v <= -(1<<13)+1:
+		buf[0] = 0x10 | (byte(*e.v>>16) & 0x0F)
+		buf[1] = byte(*e.v >> 8)
+		buf[2] = byte(*e.v)
+	case -(1<<13) <= *e.v && *e.v <= -(1<<6)+1:
+		buf[0] = 0x20 | (byte(*e.v>>8) & 0x1F)
+		buf[1] = byte(*e.v)
+	case -(1<<6) <= *e.v && *e.v <= -1:
+		buf[0] = 0x40 | (byte(*e.v) & 0x3F)
+	case 0 <= *e.v && *e.v <= (1<<6)-1:
+		buf[0] = 0x80 | byte(*e.v)
+	case (1<<6) <= *e.v && *e.v <= (1<<13)-1:
+		buf[0] = 0xC0 | byte(*e.v>>8)
+		buf[1] = byte(*e.v)
+	case (1<<13) <= *e.v && *e.v <= (1<<20)-1:
+		buf[0] = 0xE0 | byte(*e.v>>16)
+		buf[1] = byte(*e.v >> 8)
+		buf[2] = byte(*e.v)
+	case (1<<20) <= *e.v && *e.v <= (1<<27)-1:
+		buf[0] = 0xF0 | byte(*e.v>>24)
+		buf[1] = byte(*e.v >> 16)
+		buf[2] = byte(*e.v >> 8)
+		buf[3] = byte(*e.v)
+	case (1<<27) <= *e.v && *e.v <= (1<<34)-1:
+		buf[0] = 0xF8 | byte(*e.v>>32)
+		buf[1] = byte(*e.v >> 24)
+		buf[2] = byte(*e.v >> 16)
+		buf[3] = byte(*e.v >> 8)
+		buf[4] = byte(*e.v)
+	case (1<<34) <= *e.v && *e.v <= (1<<41)-1:
+		buf[0] = 0xFC | byte(*e.v>>40)
+		buf[1] = byte(*e.v >> 32)
+		buf[2] = byte(*e.v >> 24)
+		buf[3] = byte(*e.v >> 16)
+		buf[4] = byte(*e.v >> 8)
+		buf[5] = byte(*e.v)
+	case (1<<41) <= *e.v && *e.v <= (1<<48)-1:
+		buf[0] = 0xFE | byte(*e.v>>48)
+		buf[1] = byte(*e.v >> 40)
+		buf[2] = byte(*e.v >> 32)
+		buf[3] = byte(*e.v >> 24)
+		buf[4] = byte(*e.v >> 16)
+		buf[5] = byte(*e.v >> 8)
+		buf[6] = byte(*e.v)
+	case (1<<48) <= *e.v && *e.v <= (1<<55)-1:
+		buf[0] = 0xFF
+		buf[1] = byte(*e.v >> 48)
+		buf[2] = byte(*e.v >> 40)
+		buf[3] = byte(*e.v >> 32)
+		buf[4] = byte(*e.v >> 24)
+		buf[5] = byte(*e.v >> 16)
+		buf[6] = byte(*e.v >> 8)
+		buf[7] = byte(*e.v)
+	case (1 << 55) <= *e.v:
+		buf[0] = 0xFF
+		buf[1] = 0x80 | byte(*e.v>>56)
+		buf[2] = byte(*e.v >> 48)
+		buf[3] = byte(*e.v >> 40)
+		buf[4] = byte(*e.v >> 32)
+		buf[5] = byte(*e.v >> 24)
+		buf[6] = byte(*e.v >> 16)
+		buf[7] = byte(*e.v >> 8)
+		buf[8] = byte(*e.v)
+	}
+}
+func (e ordVarint64) Size() int {
+	switch {
+	case *e.v <= -(1<<55)+1:
+		return 9
+	case -(1<<55) <= *e.v && *e.v <= -(1<<48)+1:
+		return 8
+	case -(1<<48) <= *e.v && *e.v <= -(1<<41)+1:
+		return 7
+	case -(1<<41) <= *e.v && *e.v <= -(1<<34)+1:
+		return 6
+	case -(1<<34) <= *e.v && *e.v <= -(1<<27)+1:
+		return 5
+	case -(1<<27) <= *e.v && *e.v <= -(1<<20)+1:
+		return 4
+	case -(1<<20) <= *e.v && *e.v <= -(1<<13)+1:
+		return 3
+	case -(1<<13) <= *e.v && *e.v <= -(1<<6)+1:
+		return 2
+	case -(1<<6) <= *e.v && *e.v <= (1<<6)-1:
+		return 1
+	case (1<<6) <= *e.v && *e.v <= (1<<13)-1:
+		return 2
+	case (1<<13) <= *e.v && *e.v <= (1<<20)-1:
+		return 3
+	case (1<<20) <= *e.v && *e.v <= (1<<27)-1:
+		return 4
+	case (1<<27) <= *e.v && *e.v <= (1<<34)-1:
+		return 5
+	case (1<<34) <= *e.v && *e.v <= (1<<41)-1:
+		return 6
+	case (1<<41) <= *e.v && *e.v <= (1<<48)-1:
+		return 7
+	case (1<<48) <= *e.v && *e.v <= (1<<55)-1:
+		return 8
+	case (1 << 55) <= *e.v:
+		return 9
+	}
+	return -1
+}
+func (e ordVarint64) Decode(buf []byte) error {
+	if len(buf) < 1 {
+		return io.ErrUnexpectedEOF
+	}
+	switch buf[0] & 0x80 {
+	case 0:
+		switch {
+		case buf[0] == 0x00:
+			if len(buf) < 8 {
+				return io.ErrUnexpectedEOF
+			}
+			if buf[1]&0x80 == 0 {
+				if len(buf) < 9 {
+					return io.ErrUnexpectedEOF
+				}
+				*e.v = int64(uint64(0x8000000000000000) |
+					uint64(buf[1])<<56 |
+					uint64(buf[2])<<48 |
+					uint64(buf[3])<<40 |
+					uint64(buf[4])<<32 |
+					uint64(buf[5])<<24 |
+					uint64(buf[6])<<16 |
+					uint64(buf[7])<<8 |
+					uint64(buf[8]))
+			} else {
+				*e.v = int64(uint64(0xFF00000000000000) |
+					uint64(buf[1])<<48 |
+					uint64(buf[2])<<40 |
+					uint64(buf[3])<<32 |
+					uint64(buf[4])<<24 |
+					uint64(buf[5])<<16 |
+					uint64(buf[6])<<8 |
+					uint64(buf[7]))
+			}
+		case buf[0] == 0x01:
+			if len(buf) < 7 {
+				return io.ErrUnexpectedEOF
+			}
+			*e.v = int64(uint64(0xFFFF000000000000) |
+				uint64(buf[1])<<40 |
+				uint64(buf[2])<<32 |
+				uint64(buf[3])<<24 |
+				uint64(buf[4])<<16 |
+				uint64(buf[5])<<8 |
+				uint64(buf[6]))
+		case buf[0]&0xFE == 0x02:
+			if len(buf) < 6 {
+				return io.ErrUnexpectedEOF
+			}
+			*e.v = int64(uint64(0xFFFFFE0000000000) |
+				uint64(buf[0]&0x01)<<40 |
+				uint64(buf[1])<<32 |
+				uint64(buf[2])<<24 |
+				uint64(buf[3])<<16 |
+				uint64(buf[4])<<8 |
+				uint64(buf[5]))
+		case buf[0]&0xFC == 0x04:
+			if len(buf) < 5 {
+				return io.ErrUnexpectedEOF
+			}
+			*e.v = int64(uint64(0xFFFFFFFC00000000) |
+				uint64(buf[0]&0x03)<<32 |
+				uint64(buf[1])<<24 |
+				uint64(buf[2])<<16 |
+				uint64(buf[3])<<8 |
+				uint64(buf[4]))
+		case buf[0]&0xF8 == 0x08:
+			if len(buf) < 4 {
+				return io.ErrUnexpectedEOF
+			}
+			*e.v = int64(uint64(0xFFFFFFFFF8000000) |
+				uint64(buf[0]&0x07)<<24 |
+				uint64(buf[1])<<16 |
+				uint64(buf[2])<<8 |
+				uint64(buf[3]))
+		case buf[0]&0xF0 == 0x10:
+			if len(buf) < 3 {
+				return io.ErrUnexpectedEOF
+			}
+			*e.v = int64(uint64(0xFFFFFFFFFFF00000) |
+				uint64(buf[0]&0x0F)<<16 |
+				uint64(buf[1])<<8 |
+				uint64(buf[2]))
+		case buf[0]&0xE0 == 0x20:
+			if len(buf) < 2 {
+				return io.ErrUnexpectedEOF
+			}
+			*e.v = int64(uint64(0xFFFFFFFFFFFFE000) |
+				uint64(buf[0]&0x1F)<<8 |
+				uint64(buf[1]))
+		case buf[0]&0xC0 == 0x40:
+			*e.v = int64(uint64(0xFFFFFFFFFFFFFFC0) |
+				uint64(buf[0]&0x3F))
+		default:
+			return ErrInvalidVarint
+		}
+	case 0x80:
+		switch {
+		case buf[0]&0xC0 == 0x80:
+			*e.v = int64(buf[0] & 0x3F)
+		case buf[0]&0xE0 == 0xC0:
+			if len(buf) < 2 {
+				return io.ErrUnexpectedEOF
+			}
+			*e.v = int64(buf[0]&0x1F)<<8 |
+				int64(buf[1])
+		case buf[0]&0xF0 == 0xE0:
+			if len(buf) < 3 {
+				return io.ErrUnexpectedEOF
+			}
+			*e.v = int64(buf[0]&0x0F)<<16 |
+				int64(buf[1])<<8 |
+				int64(buf[2])
+		case buf[0]&0xF8 == 0xF0:
+			if len(buf) < 4 {
+				return io.ErrUnexpectedEOF
+			}
+			*e.v = int64(buf[0]&0x07)<<24 |
+				int64(buf[1])<<16 |
+				int64(buf[2])<<8 |
+				int64(buf[3])
+		case buf[0]&0xFC == 0xF8:
+			if len(buf) < 5 {
+				return io.ErrUnexpectedEOF
+			}
+			*e.v = int64(buf[0]&0x03)<<32 |
+				int64(buf[1])<<24 |
+				int64(buf[2])<<16 |
+				int64(buf[3])<<8 |
+				int64(buf[4])
+		case buf[0]&0xFE == 0xFC:
+			if len(buf) < 6 {
+				return io.ErrUnexpectedEOF
+			}
+			*e.v = int64(buf[0]&0x01)<<40 |
+				int64(buf[1])<<32 |
+				int64(buf[2])<<24 |
+				int64(buf[3])<<16 |
+				int64(buf[4])<<8 |
+				int64(buf[5])
+		case buf[0] == 0xFE:
+			if len(buf) < 7 {
+				return io.ErrUnexpectedEOF
+			}
+			*e.v = int64(buf[1])<<40 |
+				int64(buf[2])<<32 |
+				int64(buf[3])<<24 |
+				int64(buf[4])<<16 |
+				int64(buf[5])<<8 |
+				int64(buf[6])
+		case buf[0] == 0xFF:
+			if len(buf) < 8 {
+				return io.ErrUnexpectedEOF
+			}
+			if buf[1]&0x80 == 0 {
+				*e.v = int64(buf[1])<<48 |
+					int64(buf[2])<<40 |
+					int64(buf[3])<<32 |
+					int64(buf[4])<<24 |
+					int64(buf[5])<<16 |
+					int64(buf[6])<<8 |
+					int64(buf[7])
+			} else {
+				if len(buf) < 9 {
+					return io.ErrUnexpectedEOF
+				}
+				*e.v = int64(buf[1]&0x7F)<<56 |
+					int64(buf[2])<<48 |
+					int64(buf[3])<<40 |
+					int64(buf[4])<<32 |
+					int64(buf[5])<<24 |
+					int64(buf[6])<<16 |
+					int64(buf[7])<<8 |
+					int64(buf[8])
+			}
+		default:
+			return ErrInvalidVarint
+		}
+	default:
+		return ErrInvalidVarint
+	}
 	return nil
 }
 
--- encode_test.go
+++ encode_test.go
@@ -75,6 +75,84 @@ func TestOrdUvarint64(t *testing.T) {
 	})
 }
 
+func TestOrdVarint64(t *testing.T) {
+	checkEncoding := func(x int64, expected []byte) {
+		enc := New(OrdVarint64(&x))
+		b := enc.Encode()
+		require.Equal(t, expected, b)
+	}
+	checkRoundtrip := func(x int64) {
+		enc := New(OrdVarint64(&x))
+		x2 := x
+		b := enc.Encode()
+		t.Logf("%d: %s\n", x, hex.EncodeToString(b))
+		x = ^x
+		err := enc.Decode(b)
+		require.NoError(t, err)
+		require.Equal(t, x2, x)
+	}
+
+	checkOrdering := func(x int64, x2 int64) {
+		checkRoundtrip(x)
+		checkRoundtrip(x2)
+
+		enc := New(OrdVarint64(&x))
+		b := enc.Encode()
+
+		enc2 := New(OrdVarint64(&x2))
+		b2 := enc2.Encode()
+
+		require.True(
+			t,
+			bytes.Compare(b, b2) < 0,
+			"%d < %d but %s >= %s",
+			x, x2, hex.EncodeToString(b), hex.EncodeToString(b2),
+		)
+	}
+
+	checkEncoding(int64(-(1 << 63)), []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00})
+	checkEncoding(0, []byte{0x80})
+	checkEncoding(1, []byte{0x81})
+	checkEncoding(-1, []byte{0x7F})
+	checkEncoding(-2, []byte{0x7E})
+	checkEncoding(int64(1<<63-1), []byte{0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF})
+
+	checkOrdering(-125, -5)
+	checkOrdering(-64, -1)
+	checkOrdering(-32, -1)
+	checkOrdering(-576, 15)
+
+	checkOrdering(0, 1)
+	checkOrdering(1, 14)
+	checkOrdering(16, 123)
+	checkOrdering(16, 128)
+	checkOrdering(1235, 1239)
+	checkOrdering(1231, 123151)
+	checkOrdering(1231241, 1230123102)
+	checkOrdering(1<<7, 1<<14)
+	checkOrdering(1<<14, 1<<21)
+	checkOrdering(1<<21, 1<<28)
+	checkOrdering(1<<28, 1<<35)
+	checkOrdering(1<<35, 1<<42)
+	checkOrdering(1<<42, 1<<49)
+	checkOrdering(1<<49, 1<<56)
+	checkOrdering(1<<56, 1<<63-1)
+
+	trand.RandomN(t, 10000, func(t *testing.T, r *rand.Rand) {
+		x1 := int64(r.Uint64()) >> uint(r.Int()%64)
+		x2 := int64(r.Uint64()) >> uint(r.Int()%64)
+
+		if x1 == x2 {
+			x2++
+		}
+		if x2 < x1 {
+			x1, x2 = x2, x1
+		}
+
+		checkOrdering(x1, x2)
+	})
+}
+
 func BenchmarkOrdUvarint64Encode(b *testing.B) {
 	bunchaUint64s := make([]uint64, b.N)
 	for i := range bunchaUint64s {
